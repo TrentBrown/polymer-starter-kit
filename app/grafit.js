9979718ec2c5dcdc8db0776f978983d94254a71b
Trent Brown <trent@filethis.com>
2015-08-24 09:07:32 -0700

    WIP to wire a chart to API

--- app/grafit.js
+++ app/grafit.js
@@ -1,22 +1,59 @@
 function onSwaggerButtonClick()
 {
+    // Load the Swagger data
     window.swagger = new SwaggerClient
         (
             {
-                url: "http://api.gra.fit:5000/swagger.json",
+                url: "http://localhost:5000/swagger.json",
                 useJQuery: true,
-                success: function()
-                {
-                    // swagger.apis.metrics.get_metric_resource_list
-                    swagger.apis.default.get_metric_resource_list
-                    (
-                        {responseContentType: 'application/json'},
-                        function(data)
-                        {
-                            document.getElementById("mydata").innerHTML = JSON.stringify(data.obj);
-                        }
-                    );
-                }
+                success: function() { onGetSwaggerData() }
             }
         );
+}
+
+function onGetSwaggerData()
+{
+    // Get all metrics
+    window.swagger.apis.v1.get_metric_resource_list
+    (
+        {responseContentType: 'application/json'},
+        onGetMetricList
+    );
+}
+
+function onGetMetricList(response)
+{
+    data = response.obj
+    
+    // Show the raw data
+    document.getElementById("test-metrics").innerHTML = JSON.stringify(data);
+    
+    // Put data into chart
+    trent_graph = document.getElementById("test-chart");
+    trent_graph.cols = [{"label":"Month", "type":"string"}, {"label":"Days", "type":"number"}];
+    trent_graph.rows = [["January", 3],["February", 1],["March", 2]]
+    
+    // Choose the first metric
+    theMetricId = 1
+    
+    // Get all data for this metric
+    window.swagger.apis.v1.get_datum_resource_list
+    (
+        {metric_id: theMetricId},
+        {responseContentType: 'application/json'},
+        onGetDataList
+    );
+}
+
+function onGetDataList(response)
+{
+    data = response.obj
+    
+    // Show the raw data
+    document.getElementById("test-data").innerHTML = JSON.stringify(data);
+    
+    // Put data into chart
+    trent_graph = document.getElementById("test-chart");
+    trent_graph.cols = [{"label":"Month", "type":"string"}, {"label":"Days", "type":"number"}];
+    trent_graph.rows = [["January", 3],["February", 1],["March", 2]]
 }
